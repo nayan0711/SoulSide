e059c266d7d4fcd3d0ad10ffef69f6ef41024b08
AlefiyaRaja <108682831+AlefiyaRaja@users.noreply.github.com>
2024-02-28 18:42:21 +0530

    Revert "Home"

--- ios/SoulsideTests/SoulsideTests.m
+++ ios/SoulsideTests/SoulsideTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SoulSideTests : XCTestCase
+@interface SoulsideTests : XCTestCase
 
 @end
 
-@implementation SoulSideTests
+@implementation SoulsideTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
